fcbba63a708d98bb0ee43c88da68e6d4a47622b8
Oğuzhan Ercan <64038202+Oguzhanercan@users.noreply.github.com>
2021-12-29 01:52:07 +0300

    Update data_works.py

--- data_works.py
+++ data_works.py
@@ -65,7 +65,7 @@ def get_dataloader(args):
     mode = args.mode
     directory = os. getcwd()
     transform = transforms.Compose([
-        transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
+        transforms.RandomResizedCrop((args.im_size, args.im_size), scale=(0.05, 1.0)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
 
